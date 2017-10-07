b05dbed681d20abc6dd912bf6f74b674dd076bf1
Alexander Torrijos <alexander.torrijos@towerswatson.com>
2017-10-07 21:38:53 +0800

    Displays culture info from host setting

---
+++ Mosaic/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Globalization;
+
+namespace Mosaic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine(CultureInfo.CurrentCulture);
+            Console.ReadLine();
+        }
+    }
+}
